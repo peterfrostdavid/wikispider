459630ec5354ff6fa3ce546f223d156c38d43604
peterfrostdavid <peterfdavid@gmail.com>
2021-03-24 20:04:32 +0000

    WIP - initial commit

---
+++ src/main/java/pfd/wikispider/WikiSpider.java
@@ -0,0 +1,10 @@
+package pfd.wikispider;
+
+import org.wikipedia.Wiki;
+
+public class WikiSpider {
+	public static void main(String args[]) throws Exception {
+		Wiki wiki;
+		wiki = new Wiki("en.wikipedia.org");
+	}
+}
